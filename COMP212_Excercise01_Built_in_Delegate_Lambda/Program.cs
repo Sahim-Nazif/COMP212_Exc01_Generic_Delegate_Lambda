2cf1ba8a961104901e47119c7c8b8eaca9394e3d
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-01-26 21:19:54 -0500

    Add project files.

---
+++ COMP212_Excercise01_Built_in_Delegate_Lambda/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+/*
+ * TASKS:
+ * Create a C# app in which you are required to create the following methods using built-in delegates
+ * predicates and lambdas.
+ * 1- string Minimum(string1,string2,string3) which returns the smallest of three string values. To test
+ * this method, you need to use built-in Func<> delegate predicate
+ * 2- void AvgGrade(value1, value2, value3)which displays the average of three grades. To test this method,
+ * you need to use built-in Action<> delegate predicate
+ * 
+ * Here will use lambda--anonymous methods to complete this excercise which is already on my github
+ * Repository name is COMP212_Built_in_Delegate
+ */
+
+namespace COMP212_Excercise01_Built_in_Delegate_Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
